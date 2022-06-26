6fc7af9185cca0fb99a1a214c95f589aead65a92
Vasiliy566 <vasyaisaev31@gmail.com>
2022-06-26 22:01:50 +0300

    _+1

--- check_tasks.py
+++ check_tasks.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-if not os.path.isfile("1/"):
+if not os.path.isfile("./1/"):
   raise Exception("Create forlder 1/ for firts task")
 
 output = subprocess.check_output("python3 1/example.py", shell=True)
