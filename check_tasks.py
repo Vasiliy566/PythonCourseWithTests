d86be20f0e7ae6cf6d1057112f1eb5040cba3f22
Vasily <89313005745@mail.ru>
2022-06-26 21:59:00 +0300

    Update check_tasks.py

--- check_tasks.py
+++ check_tasks.py
@@ -1,3 +1,8 @@
 import os
+import subprocess
+
 if not os.path.isfile("1/"):
   raise Exception("Create forlder 1/ for firts task")
+
+output = subprocess.check_output("python3 1/example.py", shell=True)
+assert output == "Hello world", "Your program should return 'Hello World'"
